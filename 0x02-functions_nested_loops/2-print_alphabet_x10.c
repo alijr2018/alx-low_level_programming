4742743badc88c8dcd86ce12fe070913610b1e41
alijr2018 <alijr_2018@proton.me>
2023-03-21 14:17:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,6 @@
 /**
  *print_alphabet_x10 - prints the alphabet, in lowercase,10 times,
  *followed by a new line
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 
 void print_alphabet_x10(void)
@@ -22,5 +20,4 @@ void print_alphabet_x10(void)
 		k++;
 		_putchar('\n');
 	}
-	return (0);
 }
