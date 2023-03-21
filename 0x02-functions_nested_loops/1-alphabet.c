7707b08e3dbb445b1c5a043f617a401b7e534faf
alijr2018 <alijr_2018@proton.me>
2023-03-21 13:23:34 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	_putchar("abcdefghijklmnopqrstuvwxyz");
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
