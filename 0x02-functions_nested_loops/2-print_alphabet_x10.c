30b0c680e0fd3a58923790a7dd9711168dc723fa
alijr2018 <alijr_2018@proton.me>
2023-03-21 14:10:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+        int i;
+	int k;
+	k = 0;
+
+	while (k <= 9)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		k++;
+		_putchar('\n');
+	}
+}
