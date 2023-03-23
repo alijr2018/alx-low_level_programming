fc2774208393ec4a01cc8dbf4478c5f140adf9fb
alijr2018 <alijr_2018@proton.me>
2023-03-23 07:19:05 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * Description: prints
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a ;
+
+	for (a = 48 ;a <= 57 ;a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
