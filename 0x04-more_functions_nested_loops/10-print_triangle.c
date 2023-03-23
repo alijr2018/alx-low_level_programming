ca9829472a8ce67535d034b97209dd72d4f36e7f
alijr2018 <alijr_2018@proton.me>
2023-03-23 14:33:54 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - check for a digit
+ * @size : integer type
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int x;
+
+	i = 1;
+	while (i <= size && size > 0)
+	{
+		x = 0;
+		while (x < size - i)
+		{
+			_putchar(' ');
+			x++;
+		}
+		x = 0;
+		while (x < i)
+		{
+			_putchar('#');
+			x++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
