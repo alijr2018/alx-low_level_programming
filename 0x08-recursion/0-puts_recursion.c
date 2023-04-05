7f71477ceaa82fab0c4df50b18a1f8dc2fcfbaa1
alijr2018 <alijr_2018@proton.me>
2023-04-05 14:47:36 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,16 @@
  * _puts_recursion - function that prints a string, followed by new line
  * @s: string
  *
- * Return: 
+ * Return:
  **/
 
 void _puts_recursion(char *s)
 {
-	while (s[0] != '\0')
+	if (s[0] != '\0')
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
