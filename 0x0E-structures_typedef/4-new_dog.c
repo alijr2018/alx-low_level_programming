4de8b561cb60bf8a2e270d60ab8e6d2567aa81ca
alijr2018 <alijr_2018@proton.me>
2023-04-17 18:36:21 +0000

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ *
+ *
+ **/
+
+dog_t *new_dog(char *name, float age , char *owner)
+{
+	if (d)
+	{
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,4 +24,5 @@ int _putchar(char c);
 
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
