52e2175d6f7e82df75d8daca1368bca1864724d4
alijr2018 <alijr_2018@proton.me>
2023-04-17 18:29:27 +0000

    define a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _putchar(char c);
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
