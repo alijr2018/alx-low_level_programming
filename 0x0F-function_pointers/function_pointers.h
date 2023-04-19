2417fec0210dfc3101cee2df7011f18cf2c6bb3c
alijr2018 <alijr_2018@proton.me>
2023-04-19 11:19:30 +0000

    done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTION_POINTER_
 #define FUNCTION_POINTER_
 
+#include <stdio.h>
+#include<stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif /*FUNCTION_POINTER_H*/
