ffae52984828eddd2e33fc18c77b16d197e4ffe6
alijr2018 <alijr_2018@proton.me>
2023-04-19 10:24:25 +0000

    up

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include<stdlib.h>
+
 /**
  * print_name - prints a name
  * @name: name to print
@@ -8,7 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f )
+	if (name == NULL || f == NULL )
 		return;
 	(*f)(name);
 }
