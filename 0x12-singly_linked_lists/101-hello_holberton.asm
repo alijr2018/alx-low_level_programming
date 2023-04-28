49bdcd160c0cf2d0d6a8e1049af6a965cb2afba1
alijr2018 <alijr_2018@proton.me>
2023-04-28 13:48:58 +0000

    up

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,19 @@
-
 section .data
-prompt  db 'Name: ', 0          ;; char prompt[] = "Name: ";
-result  db 'Hello, %s!', 0      ;; char result[] = "Hello, %s!";
+message  db "Hello, Holberton", 0 ;message
+result  db "%s", ,10,0 ; format
 
-    section .bss
-name    resb 1024       ;; char name[24];
+    section .text ; code section
+global main
 
-    section .text
-global _main
-extern _printf, _gets
-_main:
+extern printf ;call function printf 
+
+main:
     push prompt
-    call _printf        ;; printf(prompt);
-    add esp, 4
-
-    push name
-    call _gets          ;; gets(name);
-    add esp, 4
-
-    push name
-    push result
-    call _printf        ;; printf(result, name);
-    add esp, 8
-
-    xor eax, eax        ;; 0 for a return value
+    call printf        
+    
+    mov     rdi,fmt
+    mov     rsi,msg
+    mov     rax,0
+    pop     rbp             
+    mov     rax,0
     ret
