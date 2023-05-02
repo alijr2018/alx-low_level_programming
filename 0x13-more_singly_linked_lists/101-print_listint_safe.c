51593e204a658dd36509d02176ca9744d1b5b2f4
alijr2018 <alirami120@hotmail.fr>
2023-05-02 21:38:27 +0100

    done

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,51 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "lists.h"
+#include"lists.h"
 
 /**
- * print_listint_safe - Prints a linked list
- * @head: Pointer to head of list
- *
- * Return: The number of nodes in the list
- */
+ * print_listint_safe - function that prints a listint_t linked list.
+ * @head: pointer.
+ * Return: the number of nodes in the list.
+*/
+
+
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *turtle, *hare;
-    size_t count = 0;
+	size_t i;
+	long int j;
 
-    if (!head)
-        exit(98);
-
-    turtle = head;
-    hare = head;
-
-    while (hare && hare->next)
-    {
-        printf("[%p] %d\n", (void *)turtle, turtle->n);
-        count++;
-        turtle = turtle->next;
-        hare = hare->next->next;
-
-        if (turtle == hare)
-        {
-            turtle = head;
-            while (turtle != hare)
-            {
-                printf("[%p] %d\n", (void *)turtle, turtle->n);
-                count++;
-                turtle = turtle->next;
-                hare = hare->next;
-            }
-
-            printf("[%p] %d\n", (void *)turtle, turtle->n);
-            count++;
-            printf("-> [%p] %d\n", (void *)hare, hare->n);
-            exit(98);
-        }
-    }
-
-    printf("[%p] %d\n", (void *)turtle, turtle->n);
-    count++;
-
-    return (count);
+	if (!head)
+		exit(98);
+	i = 0;
+	while (head)
+	{
+		j = head - head->next;
+		i++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (j > 0)
+			head = head->next;
+		else
+		{
+			printf("->[%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (j);
 }
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,9 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	i = malloc(sizeof(listint_t));
 	if (i == NULL)
-	{
 		return (NULL);
-	}
+
 	i->n = n;
 	i->next = *head;
 	*head = i;
