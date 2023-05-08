3f26139396b821f90f8552587605fb4f284434cf
alijr2018 <alirami120@hotmail.fr>
2023-05-08 22:49:34 +0100

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ *@filename: pointer
+ *@text_content: pointer
+ *Return: 1 on success, -1 on failure .
+ **/
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, j, k;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (i == -1)
+		return (i);
+
+	k = 0;
+	while  (text_content[k])
+	{
+		j = write(i, text_content, k);
+		k++;
+	}
+	if (j == -1)
+		return (-1);
+
+	close(i);
+	return (1);
+}
+
