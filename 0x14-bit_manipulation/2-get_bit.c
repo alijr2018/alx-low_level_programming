bd31d81dda97829c34a5d7c841175874cbc124b8
alijr2018 <alirami120@hotmail.fr>
2023-05-25 01:48:22 +0100

    done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,18 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > (sizeof(n) * 8))
+	{
 		return (-1);
+	}
+
 	n = n >> index;
+
 	if (n & 1)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
