816015a3ec2f319a1dcc46400b3d3c937bff36ee
alijr2018 <alijr_2018@proton.me>
2023-06-15 13:22:16 +0100

    done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,9 +4,9 @@
  * add_dnodeint - function that adds a new node at the beginning,
  * of a dlistint_t list.
  * @head: double pointer to the head pointer of a doubly linked list.
- * @n: int
- * Return: the address of the new element, or NULL if it failed
-*/
+ * @n: int.
+ * Return: the address of the new element, or NULL if it failed.
+**/
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
