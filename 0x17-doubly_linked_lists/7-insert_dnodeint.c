5a584395945d6b9014f396c0a545f3a84f8fac29
alijr2018 <alijr_2018@proton.me>
2023-06-15 16:12:55 +0100

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,42 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *n_node, *i;
-	unsigned int j;
+	dlistint_t *n_done, *k;
+	unsigned int i;
 
-	i = *h;
-	j = 0;
+	k = *h;
+	i = 0;
 
 	if (h == NULL)
 		return (NULL);
-
+	n_done = malloc(sizeof(dlistint_t));
+	if (n_done == NULL)
+		return (NULL);
+	n_done->n = n;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	n_node = malloc(sizeof(dlistint_t));
-
-	while (i != NULL && j < idx - 1)
 	{
-		i = i->next;
-		j++;
+		n_done->prev = NULL;
+		n_done->next = *h;
+		if (*h != NULL)
+			(*h)->prev = n_done;
+		*h = n_done;
+		return (n_done);
 	}
-	if (i == NULL)
+	while (k != NULL && i < idx - 1)
+	{
+		k = k->next;
+		i++;
+	}
+	if (k == NULL)
+	{
+		free(n_done);
 		return (NULL);
+	}
+	n_done->prev = k;
+	n_done->next = k->next;
+	if (k->next != NULL)
+		k->next->prev = n_done;
 
-	if (i->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	if (n_node == NULL)
-		return (NULL);
-
-	n_node->n = n;
-	n_node->prev = i;
-	n_node->next = i->next;
-	i->next->prev = n_node;
-	i->next = n_node;
-	return (n_node);
+	k->next = n_done;
+	return (n_done);
 }
