c63ea1ab6fad4f1f51675536dba1619ca56a8c37
alijr2018 <alijr_2018@proton.me>
2023-10-10 20:13:49 +0100

    done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,5 +5,36 @@
  * @array:
  * @size:
  * @value:
- * Retuen:
- **
+ * Return:
+ **/
+
+
+int binary_search(int *array, size_t size, int value) {
+    if (array == NULL)
+        return -1;
+
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        printf("Searching in array: ");
+        for (size_t i = left; i <= (size_t)right; i++) {
+            if (i < size - 1)
+                printf("%d, ", array[i]);
+            else
+                printf("%d\n", array[i]);
+        }
+
+        if (array[mid] == value)
+            return mid;
+
+        if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
