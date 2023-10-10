40ae173cb3ad769d485af8ded4d9b919105fa32c
alijr2018 <alijr_2018@proton.me>
2023-10-10 19:18:05 +0100

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,23 @@
 #include"search_nlogs.h"
+
+/**
+ *a function that searches for a value in an array of integers using the Linear search algorithm
+ * @array:
+ * @size:
+ * @value:
+ * Return:
+*/
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        printf("Value checked array[%lu] = [%d]\n", 0, -1);
+        return -1;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1;
+}
